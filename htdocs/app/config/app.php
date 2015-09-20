60544059800289c8c931f1200a2243303f6dd0c7
mav.peri <mav.peri@holidaylettings.co.uk>
2015-09-20 10:17:01 +0100

    change key

--- htdocs/app/config/app.php
+++ htdocs/app/config/app.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'debug' => false,
+	'debug' => true,
 
 	/*
 	|--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'dsa93hdsalsd3',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 

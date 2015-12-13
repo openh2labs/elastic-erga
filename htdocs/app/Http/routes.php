c4b1c848c6a6d90abd6f9860d9706ba136a28892
mav.peri <mav.peri@holidaylettings.co.uk>
2015-12-13 12:49:03 +0000

    add seeder data

--- htdocs/app/Http/routes.php
+++ htdocs/app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user/{id}', 'UserController@showProfile');
+
+Route::get('alert/home', 'AlertController@home');
--- htdocs/database/seeds/DatabaseSeeder.php
+++ htdocs/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,9 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+        $this->call('alertsSeeder');
+
+        $this->command->info('alerts table seeded!');
 
         Model::reguard();
     }

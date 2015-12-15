53058b5453a09fd15a4784bb99d93b4a63134537
mav.peri <mav.peri@holidaylettings.co.uk>
2015-12-15 23:34:32 +0000

    progress test setup

---
+++ htdocs/app/Http/Controllers/PostController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mavperi
+ * Date: 14/12/15
+ * Time: 12:09
+ */
---
+++ htdocs/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mavperi
+ * Date: 14/12/15
+ * Time: 11:50
+ */

dcb737be637f5a3090591bc17a6682422d2d2599
mav.peri <mav.peri@holidaylettings.co.uk>
2016-01-07 15:24:43 +0000

    improve the email notification

--- htdocs/resources/views/email_alert.blade.php
+++ htdocs/resources/views/email_alert.blade.php
@@ -6,7 +6,8 @@
  * Time: 09:52
  */
 
-        ?>
-You asked to be notified when <?php echo $description ?>
-
-Powered by elastic-erga
+?>
+Hey,
+<p>You asked to be notified when <?php echo $description ?></p>
+<hr>
+<p><a href="https://github.com/openh2labs/elastic-erga/wiki">Powered by elastic-erga</a></p>

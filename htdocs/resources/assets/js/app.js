3c09f8a76949a9ddf150476d0cedc924f57e9458
Alexandros Spyropoulos <spyropoulos.a@gmail.com>
2016-02-23 12:33:05 +0000

    Tidy up lint alerts

--- htdocs/resources/assets/js/app.js
+++ htdocs/resources/assets/js/app.js
@@ -8,5 +8,4 @@ require('bootstrap-less');
 window.elastic_erga_app = (function($){
     "use strict";
     var ergaTerminalView = require('./terminal/terminal_factory').create($, window.console);
-
 })(window.jQuery);

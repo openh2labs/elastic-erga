5a140a3dbd981b2c52add8ebfc99716b4b1e62c9
Vaughn Angoy <vaughn8@googlemail.com>
2016-06-29 07:38:18 +0100

    bug fixes from master

--- devops/docker/app/bootstrap.sh
+++ devops/docker/app/bootstrap.sh
@@ -8,11 +8,11 @@ echo 'Bootstrapping elastic erga ..'
 # set working directory
 cd /var/www/elastic-erga
 
+# install npm packages
+npm install
+
 # install composer packages
 composer install -o
 
 # run gulp tasks
 gulp javascript
-
-# install npm packages
-npm install

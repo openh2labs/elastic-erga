15ccb9c528a3ffa74ae5d349a859e35bc45a49b9
Vaughn Angoy <vaughn8@googlemail.com>
2016-06-30 12:35:47 +0100

    adding CORS for devolopment

--- htdocs/public/index.php
+++ htdocs/public/index.php
@@ -35,6 +35,9 @@ require __DIR__.'/../bootstrap/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
 /*
 |--------------------------------------------------------------------------
 | Run The Application

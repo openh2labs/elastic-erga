ca68e2d85261ae9d7873fa8eb4fea793ae1e814b
purificant <purificant@users.noreply.github.com>
2016-07-06 14:56:47 +0100

    adjust build process

--- devops/docker/app/bootstrap.sh
+++ devops/docker/app/bootstrap.sh
@@ -6,7 +6,7 @@ set -o pipefail
 echo 'Bootstrapping elastic erga ..'
 
 # set working directory
-cd /var/www/elastic-erga
+cd /var/www/elastic-erga/laravel
 
 # install composer packages
 composer install -o
@@ -15,4 +15,10 @@ composer install -o
 npm install
 
 # run gulp tasks
-gulp javascript
+gulp
+# todo: deprecate gulp
+
+cd /var/www/elastic-erga/react-boilerplate
+
+npm install
+npm run build

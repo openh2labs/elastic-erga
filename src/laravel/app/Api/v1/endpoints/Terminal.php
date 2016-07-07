ff94d582b519139a9e5cad1697ad279757d9bff5
purificant <purificant@users.noreply.github.com>
2016-07-07 20:15:29 +0100

    hook up api to live search

--- src/laravel/app/Api/v1/endpoints/Terminal.php
+++ src/laravel/app/Api/v1/endpoints/Terminal.php
@@ -16,8 +16,56 @@ class Terminal extends BaseEndpoint
     {
         $q = $request->input('q');
 
-        $ret = (new Mock())->generateMockData($q);
+        // uncomment to revert to mock data
+        //$ret = (new Mock())->generateMockData($q);
+        //return response()->json($ret);
 
+        $util = new ElasticUtil();
+        $match = '{
+        "size" : 50,
+        "query" : {
+        "match" : { REPLACE }
+    }
+    }';
+
+        $matchAll = '{
+        "size" : 50,
+        "query" : {
+        "match_all" : { }
+    }
+    }';
+
+        if (empty($q)) {
+            $query = $matchAll;
+        } else {
+            $query = str_replace('REPLACE', '"_all":"'.$q.'"', $match);
+        }
+
+        $data = $util->getTerminalData(json_decode($query, true));
+
+        foreach ($data['hits'] as $item) {
+            $source = $item['_source'];
+
+            // TODO: write an adapter for different data types
+            if (! isset($source['syslog_message'])) {
+                // filter out everything non syslog
+                // TODO: move pre-feiltering to elastic query
+                continue;
+            }
+
+            $syslogMessage = json_decode($source['syslog_message']);
+            if ($syslogMessage === null) {
+                // TODO: handle decodibng failure
+                continue;
+            }
+
+            $message = [
+                'message' => $syslogMessage,
+            ];
+
+            $ret[] = $message;
+        }
+        
         return response()->json($ret);
     }
 }

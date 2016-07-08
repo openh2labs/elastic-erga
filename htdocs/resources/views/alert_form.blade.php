0d4da6817809c57144e57738e94d76c8b42d9a7f
Vaughn Angoy <vaughn8@googlemail.com>
2016-07-08 12:05:54 +0100

    revamping alert form

--- htdocs/resources/views/alert_form.blade.php
+++ htdocs/resources/views/alert_form.blade.php
@@ -24,101 +24,137 @@
         {!! Form::hidden('id', $alert->id) !!}
     @endif
 
-<p>
-        {!! Form::label('description', 'alert description') !!}
-        {!! Form::input('text', 'description', null, ['size' => '75'])  !!}
-    </p>
-    <p>
-        {!! Form::label('criteria', 'alert query (json)') !!}
-        {!! Form::textarea('criteria', null, ['cols'=>75, 'rows'=>20]) !!}
-    </p>
-    <p>
-        {!! Form::label('criteria_total', 'total query (json)') !!}
-        {!! Form::textarea('criteria_total', null, ['cols'=>75, 'rows'=>20]) !!}
-    </p>
-    <p>
-    <div id="prefetch">
-            {!! Form::label('es_host', 'es_host') !!}
-            {!! Form::input('text', 'es_host', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}
-         </div>
-    </p>
-    <p>
-         <div id="prefetch_indexes">
-        {!! Form::label('es_index', 'es_index') !!}
-        {!! Form::input('es_index', 'es_index', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!} supported wild cards are %Y% (year in YYYY format), %m% (month 01 to 12) and %d% (day 01 to 31)
+    <div class="row description">
+        <div class="col-xs-12">
+            <div class="round">{!! Form::input('text', 'description', null, ['size' => '75', 'placeholder' => 'enter alert description'])  !!}</div>
         </div>
-    </p>
-    <p>
-        <div id="prefetch_types">
-             {!! Form::label('es_index', 'es_type') !!}
-            {!! Form::input('es_index', 'es_type', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}
-        </div>
-    </p>
-    <p>
-        <div id="prefetch_es_date_time_field">
-            {!! Form::label('es_datetime_field', 'es_datetime_field') !!}
-            {!! Form::input('es_datetime_field', 'es_datetime_field', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}
-        </div>
-    </p>
-    <p>
-        {!! Form::label('minutes_back', 'minutes_back') !!}
-        {!! Form::input('minutes_back', 'minutes_back', null, ['size' => '10'])  !!} How many minutes back you want to be checking
-    </p>
-    <p>
-        {!! Form::label('pct_of_total_threshold', 'pct_of_total_threshold') !!}
-        {!! Form::input('pct_of_total_threshold', 'pct_of_total_threshold', null, ['size' => '10'])  !!}%
-    </p>
-    <p>
-        {!! Form::label('number_of_hits', 'number_of_hits') !!}
-        {!! Form::input('number_of_hits', 'number_of_hits', null, ['size' => '10'])  !!}
-    </p>
-    <p>
-        <div id="prefetch_alert_email_sender">
-            {!! Form::label('alert_email_sender', 'alert_email_sender') !!}
-            {!! Form::input('alert_email_sender', 'alert_email_sender', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!} (if smtp make sure its a permitted sender)
-        </div>
-    </p>
-    <p>
-        <div id="prefetch_alert_email_recipient">
-          {!! Form::label('alert_email_recipient', 'alert_email_recipient') !!}
-            {!! Form::input('alert_email_recipient', 'alert_email_recipient', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}
-        </div>
-    </p>
-    <p>
-        {!! Form::label('alert_type', 'alert_type') !!}
-        {!! Form::input('alert_type', 'alert_type', null, ['size' => '50'])  !!} e0 or gt0
-    </p>
-    <p>
-        {!! Form::label('consecutive_failures', 'consecutive_failures') !!}
-        {!! Form::input('consecutive_failures', 'consecutive_failures', null, ['size'=>'5']) !!} number of consecutive failures before hit alerts are sent
-    </p>
-    <p>
-        {!! Form::label('consecutive_failures_pct', 'consecutive_failures_pct') !!}
-        {!! Form::input('consecutive_failures_pct', 'consecutive_failures_pct', null, ['size'=>'5']) !!} number of consecutive failures before pct alerts are sent
-    </p>
-    <p>
-        {!! Form::label('alert_enabled_gt0', 'alert_enabled_gt0') !!}
-        {!! Form::input('alert_enabled_gt0', 'alert_enabled_gt0', null, ['size'=>'5']) !!} hits greater than zero alerts (1=enabled, 0=disabled)
-    </p>
-    <p>
-        {!! Form::label('alert_enabled_gt0_pct', 'alert_enabled_gt0_pct') !!}
-        {!! Form::input('alert_enabled_gt0_pct', 'alert_enabled_gt0_pct', null, ['size'=>'5']) !!} percentage greater than zero alerts (1=enabled, 0=disabled)
-    </p>
-    <p>
-        {!! Form::label('alert_enabled_e0', 'alert_enabled_e0') !!}
-        {!! Form::input('alert_enabled_e0', 'alert_enabled_e0', null, ['size'=>'5']) !!} zero hit alerts (1=enabled, 0=disabled)
-    </p>
+    </div>
 
+    <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#elastic-search" aria-controls="elastic-search" role="tab"
+                                                  data-toggle="tab">Elastic search query setup</a></li>
+        <li role="presentation"><a href="#alerts" aria-controls="alerts" role="tab" data-toggle="tab">Alert setup</a>
+        </li>
+        <li role="presentation"><a href="#misc" aria-controls="misc" role="tab" data-toggle="tab">Misc</a></li>
+    </ul>
+
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="elastic-search">
+            {{-- elastic search setup --}}
+            <div class="row">
+                <div class="col-xs-12 col-md-6">
+                    <div class="row">
+                        <div class="col-xs-12">{!! Form::label('criteria', 'alert query (json)') !!}</div>
+                        <div class="col-xs-12">{!! Form::textarea('criteria', null, ['cols'=>75, 'rows'=>20]) !!}</div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <div class="row">
+                        <div class="col-xs-12">{!! Form::label('criteria_total', 'total query (json)') !!}</div>
+                        <div class="col-xs-12">{!! Form::textarea('criteria_total', null, ['cols'=>75, 'rows'=>20]) !!}</div>
+                    </div>
+                </div>
+                <div class="col-xs-12">
+                    <div id="prefetch" class="row">
+                        <div class="col-xs-12 col-md-4">{!! Form::label('es_host', 'host') !!}</div>
+                        <div class="col-xs-12 col-md-8">{!! Form::input('text', 'es_host', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}</div>
+                    </div>
+                    <div id="prefetch_indexes" class="row">
+                        <div class="col-xs-12 col-md-4">{!! Form::label('es_index', 'index') !!}</div>
+                        <div class="col-xs-12 col-md-8">{!! Form::input('es_index', 'es_index', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}
+                            <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="supported wildcards are %Y% (year in YYYY format), %m% (month 01 to 12) and %d% (day 01 to 31)"></i>
+                        </div>
+                    </div>
+                    <div id="prefetch_types" class="row">
+                        <div class="col-xs-12 col-md-4">{!! Form::label('es_index', 'type') !!}</div>
+                        <div class="col-xs-12 col-md-8">{!! Form::input('es_index', 'es_type', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="alerts">
+            <div id="prefetch_alert_email_sender" class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('alert_email_sender', 'alert email sender') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('alert_email_sender', 'alert_email_sender', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}
+                    (if smtp make sure its a permitted sender)
+                </div>
+            </div>
+            <div id="prefetch_alert_email_recipient" class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('alert_email_recipient', 'alert email recipient') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('alert_email_recipient', 'alert_email_recipient', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('alert_type', 'alert type') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('alert_type', 'alert_type', null, ['size' => '50'])  !!}
+                    e0
+                    or gt0
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('consecutive_failures', 'consecutive failures') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('consecutive_failures', 'consecutive_failures', null, ['size'=>'5']) !!}
+                    <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="number of consecutive failures before hit alerts are sent"></i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('consecutive_failures_pct', 'consecutive failures pct') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('consecutive_failures_pct', 'consecutive_failures_pct', null, ['size'=>'5']) !!}
+                    <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="number of consecutive failures before pct alerts are sent"></i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('alert_enabled_gt0', 'alert enabled gt0') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('alert_enabled_gt0', 'alert_enabled_gt0', null, ['size'=>'5']) !!}
+                    <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="hits greater than zero alerts (1=enabled, 0=disabled)"></i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('alert_enabled_gt0_pct', 'alert enabled gt0 pct') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('alert_enabled_gt0_pct', 'alert_enabled_gt0_pct', null, ['size'=>'5']) !!}
+                    <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="percentage greater than zero alerts (1=enabled, 0=disabled)"></i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('alert_enabled_e0', 'alert enabled e0') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('alert_enabled_e0', 'alert_enabled_e0', null, ['size'=>'5']) !!}
+                    <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="zero hit alerts (1=enabled, 0=disabled)"></i>
+                </div>
+            </div>
+        </div>
+        <div role="tabpanel" class="tab-pane" id="misc">
+            <div id="prefetch_es_date_time_field" class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('es_datetime_field', 'datetime_field') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('es_datetime_field', 'es_datetime_field', null, ['size' => '50', 'class'=>'typeahead tt-query',  'autocomplete'=>'off', 'spellcheck'=>'false'])  !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('minutes_back', 'minutes back') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('minutes_back', 'minutes_back', null, ['size' => '10'])  !!}
+                    <i class="fa fa-question-circle-o" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="How many minutes back you want to be checking"></i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('pct_of_total_threshold', 'pct of total threshold') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('pct_of_total_threshold', 'pct_of_total_threshold', null, ['size' => '10'])  !!}
+                    %
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-4">{!! Form::label('number_of_hits', 'number of hits') !!}</div>
+                <div class="col-xs-12 col-md-8">{!! Form::input('number_of_hits', 'number_of_hits', null, ['size' => '10'])  !!}</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row submit">
+        <div class="col-xs-12">
     @if ($type === "create")
-        <p>
-            {!! Form::submit('create new alert monitor!') !!}
-        </p>
+            {!! Form::submit('create new alert monitor!', ['class' => 'btn btn-primary btn-lg']) !!}
     @elseif ($type === "edit")
-        <p>
             {!! Form::submit('save changes') !!}
-        </p>
     @endif
     {!!  Form::close() !!}
+        </div>
+    </div>
     @if(isset($librato))
         @if($librato != null)
             <a href="/librato/edit/{!! $alert->id !!}">Edit Librato integration</a>
@@ -133,3 +169,9 @@
 @endsection
 
 @section('active_tab_alert_list') active @endsection
+
+@section('js')
+    $(".nav-tabs a").tab();
+    $('[data-toggle="tooltip"]').tooltip();
+@endsection
+
--- htdocs/resources/views/master_layout.blade.php
+++ htdocs/resources/views/master_layout.blade.php
@@ -11,9 +11,11 @@ $server_host = $_SERVER['HTTP_HOST'];
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>@yield('title')</title>
-
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <!-- Load All CSS from a single concatenated file. See resources/assests/less/app.less  -->
     <link rel="stylesheet" href="{{ elixir("css/app.css") }}" />
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -21,6 +23,8 @@ $server_host = $_SERVER['HTTP_HOST'];
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script   src="https://code.jquery.com/jquery-1.12.4.min.js"   integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="   crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </head>
 <body>
 
@@ -42,7 +46,9 @@ $server_host = $_SERVER['HTTP_HOST'];
 
 
 <!-- Load All JS from a single concatenated file, see gulpfile.js and resources/assets/js/app.js -->
-<script src="{{ elixir("js/app.js") }}"></script>
+<script>
+    @yield('js')
+</script>
 
 </body>
 </html>

4206c6c284a02a5510fe2297ac71a719ab505666
Vaughn Angoy <vaughn8@googlemail.com>
2016-07-12 13:56:36 +0100

    css cleanup of dashboard table

--- src/laravel/resources/views/alerts_home.blade.php
+++ src/laravel/resources/views/alerts_home.blade.php
@@ -17,27 +17,28 @@ $b = $_SERVER['HTTP_HOST'];
 
 @section('main')
     <h1>Alerts dashboard : {{ $title }} </h1>
-
-    <h2>legend</h2>
-    <dl class="clearfix">
-        <dt><i class="fa fa-exclamation-triangle" aria-hidden="true" title="alert type"></i></dt>
-        <dd>alert type</dd>
-        <dt><i class="fa fa-percent" aria-hidden="true" title="greater than percentage"></i></dt>
-        <dd>greater than percentage</dd>
-        <dt><i class="fa fa-heartbeat" aria-hidden="true" title="greater than hit count"></i></dt>
-        <dd>greater than hit count</dd>
-        <dt><i class="fa fa-line-chart" aria-hidden="true" title="zero results"></i></dt>
-        <dd>zero results</dd>
-        <dt><i class="fa fa-cogs" aria-hidden="true" title="ES connection configuration"></i></dt>
-        <dd>ES connection configuration</dd>
-    </dl>
+    <section class="hide-in-desktop hide-in-handheld">
+        <h2>legend</h2>
+        <dl class="clearfix">
+            <dt><i class="fa fa-exclamation-triangle" aria-hidden="true" title="alert type"></i></dt>
+            <dd>alert type</dd>
+            <dt><i class="fa fa-percent" aria-hidden="true" title="greater than percentage"></i></dt>
+            <dd>greater than percentage</dd>
+            <dt><i class="fa fa-heartbeat" aria-hidden="true" title="greater than hit count"></i></dt>
+            <dd>greater than hit count</dd>
+            <dt><i class="fa fa-line-chart" aria-hidden="true" title="zero results"></i></dt>
+            <dd>zero results</dd>
+            <dt><i class="fa fa-cogs" aria-hidden="true" title="ES connection configuration"></i></dt>
+            <dd>ES connection configuration</dd>
+        </dl>
+    </section>
 
     <div class="dashboard container-fluid">
         <div class="row head">
             <div class="col-xs-6 col-sm-4">
                 <div class="row">
-                    <div class="col-xs-1"><span class="number">id</span></div>
-                    <div class="col-xs-9">description</div>
+                    <div class="col-xs-3 center">id</div>
+                    <div class="col-xs-9 left">description</div>
                 </div>
             </div>
             <div class="col-xs-6 col-sm-8">
@@ -46,7 +47,7 @@ $b = $_SERVER['HTTP_HOST'];
                         <span class="hide-in-mobile">alert type</span>
                         <span class="bc"><i class="fa fa-exclamation-triangle" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="alert type"></i></span>
                     </div>
-                    <div class="col-xs-2 relative">
+                    <div class="col-xs-2 col-md-3 relative">
                         <span class="hide-in-mobile">greater than percentage</span>
                         <span class="bc"><i class="fa fa-percent" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="greater than percentage"></i></span>
                     </div>
@@ -70,8 +71,8 @@ $b = $_SERVER['HTTP_HOST'];
             <div class="row list">
                 <div class="col-xs-6 col-sm-4">
                     <div class="row">
-                        <div class="col-xs-1"><span class="badge number">{{ $alert->id }}</span></div>
-                        <div class="col-xs-11 desc">
+                        <div class="col-xs-3 center"><span class="badge number">{{ $alert->id }}</span></div>
+                        <div class="col-xs-9 left">
                             @if ($alert->description === "")
                                 <a href="/alert/edit/{{$alert->id }}">no description</a>
                             @else
@@ -81,120 +82,56 @@ $b = $_SERVER['HTTP_HOST'];
                     </div>
                 </div>
                 <div class="col-xs-6 col-sm-8">
-                    <div class="col-xs-2 col-md-2">
-                        {{ $alert->alert_type }}
-                    </div>
-                    <div class="col-xs-2 col-md-3 relative">
-                        @if($alert->alert_type === "gt0" )
-                            @if($alert->pct_alert_state === 1)
-                                <span class="bc"><span class="badge red">{{ $alert->pct_of_total_threshold  }}%</span></span>
+                    <div class="row">
+                        <div class="col-xs-2">
+                            {{ $alert->alert_type }}
+                        </div>
+                        <div class="col-xs-2 col-md-3 relative">
+                            @if($alert->alert_type === "gt0" )
+                                @if($alert->pct_alert_state === 1)
+                                    <span class="bc"><span class="badge red">{{ $alert->pct_of_total_threshold  }}%</span></span>
+                                @else
+                                    <span class="bc"><span class="badge green">{{ $alert->pct_of_total_threshold  }}%</span></span>
+                                @endif
                             @else
-                                <span class="bc"><span class="badge green">{{ $alert->pct_of_total_threshold  }}%</span></span>
+                                <span class="bc"><i class="fa fa-circle" aria-hidden="true"></i></span>
                             @endif
-                        @else
-                            <span class="bc"><i class="fa fa-circle" aria-hidden="true"></i></span>
-                        @endif
-                    </div>
-                    <div class="col-xs-2 col-md-2 relative">
-                        @if($alert->alert_type === "gt0" && $alert->number_of_hits > 0)
-                            @if($alert->number_hit_alert_state === 1)
-                                <span class="bc"><span class="badge red">{{ $alert->number_of_hits  }}</span></span>
+                        </div>
+                        <div class="col-xs-2 relative">
+                            @if($alert->alert_type === "gt0" && $alert->number_of_hits > 0)
+                                @if($alert->number_hit_alert_state === 1)
+                                    <span class="bc"><span class="badge red">{{ $alert->number_of_hits  }}</span></span>
+                                @else
+                                    <span class="bc"><span class="badge green">{{ $alert->number_of_hits  }}</span></span>
+                                @endif
                             @else
-                                <span class="bc"><span class="badge green">{{ $alert->number_of_hits  }}</span></span>
+                                <span class="bc"><span class="badge grey">N/A</span></span>
                             @endif
-                        @else
-                            <span class="bc"><span class="badge grey">N/A</span></span>
-                        @endif
-                    </div>
-                    <div class="col-xs-2 col-md-2 relative">
-                        @if($alert->alert_type === "e0" )
-                            @if($alert->zero_hit_alert_state === 1)
+                        </div>
+                        <div class="col-xs-2 relative">
+                            @if($alert->alert_type === "e0" )
+                                @if($alert->zero_hit_alert_state === 1)
+                                    <span class="bc"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>
+                                @else
+                                    <span class="bc"><i class="fa fa-check-circle" aria-hidden="true"></i></span>
+                                @endif
+
+                            @else
+                                N/A
+                            @endif
+                        </div>
+                        <div class="col-xs-3 col-md-3 relative">
+                            @if($alert->es_config_error_state === 1)
                                 <span class="bc"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>
                             @else
                                 <span class="bc"><i class="fa fa-check-circle" aria-hidden="true"></i></span>
                             @endif
-
-                        @else
-                            N/A
-                        @endif
-                    </div>
-                    <div class="col-xs-3 col-md-2 relative">
-                        @if($alert->es_config_error_state === 1)
-                            <span class="bc"><i class="fa fa-exclamation-circle" aria-hidden="true"></i></span>
-                        @else
-                            <span class="bc"><i class="fa fa-check-circle" aria-hidden="true"></i></span>
-                        @endif
+                        </div>
                     </div>
                 </div>
             </div>
         @endforeach
     </div>
-
-    <!-- <table class="table table-striped table-hover table-condensed">
-        <tr>
-            <th>id</th>
-            <th>description</th>
-            <th class="text-center">alert type</th>
-            <th class="text-center">greater<br>than<br>percentage</th>
-            <th class="text-center">greater<br>than hit<br>count</th>
-            <th class="text-center">zero results</th>
-            <th class="text-center">ES connection<br>configuration</th>
-        </tr>
-        @foreach($alerts as $alert)
-        <tr>
-            <td>{{ $alert->id }}</td>
-            <td>
-                @if ($alert->description === "")
-                    <a href="/alert/edit/{{$alert->id }}">no description</a>
-                @else
-                    <a href="/alert/edit/{{$alert->id }}">{{ $alert->description  }}</a>
-                @endif
-               </td>
-            <td class="text-center">{{ $alert->alert_type  }}</td>
-            <td class="text-center">
-                @if($alert->alert_type === "gt0" )
-                    @if($alert->pct_alert_state === 1)
-                        <span class="label label-danger"> {{ $alert->pct_of_total_threshold  }}%</span>
-                    @else
-                        <span class="label label-success">{{ $alert->pct_of_total_threshold }}%</span>
-                    @endif
-                @endif
-            </td>
-            <td class="text-center">
-                @if($alert->alert_type === "gt0" && $alert->number_of_hits > 0)
-                    @if($alert->number_hit_alert_state === 1)
-                        <span class="label label-danger"> {{ $alert->number_of_hits  }}</span>
-                    @else
-                        <span class="label label-success">{{ $alert->number_of_hits }}</span>
-                    @endif
-                @else
-                    N/A
-                @endif
-            </td>
-            <td class="text-center">
-                @if($alert->alert_type === "e0" )
-                    @if($alert->zero_hit_alert_state === 1)
-                        <span class="label label-danger">0 found</span>
-                    @else
-                        <span class="label label-success">OK</span>
-                    @endif
-
-                @else
-                    N/A
-                @endif
-            </td>
-            <td class="text-center">
-                    @if($alert->es_config_error_state === 1)
-                        <span class="label label-danger">error</span>
-                    @else
-                        <span class="label label-success">OK</span>
-                    @endif
-            </td>
-        </tr>
-        @endforeach
-    </table> -->
-
-
 
     <hr>
     <ul>

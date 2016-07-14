128fc0d25eb60b8f1e4add4c1041a1cc2d3d6375
Vaughn Angoy <vaughn8@googlemail.com>
2016-07-14 10:25:20 +0100

    updating config references that are supposed to come from PHP

--- src/react-boilerplate/index.js
+++ src/react-boilerplate/index.js
@@ -9,11 +9,27 @@ import reducer from './reducers';
 import { init as initActions, getTerminalData } from './actions';
 
 import App from './components/app';
-import Index from './components/layouts/index';
+import Index from './components/terminal/index';
 
 const store = createStore(reducer, {},
 	compose(applyMiddleware(reduxThunk),
 	window.devToolsExtension ? window.devToolsExtension() : f => f));
+let attachTo;
+let terminalURL;
+
+try {
+    terminalURL = componentConfig.terminal.TERMINAL_URL
+}catch(e){
+    console.error(e);
+    console.error('Warning: componentConfig.terminal.TERMINAL_URL not set. Please ensure PHP is serving the config correctly.');
+}
+
+try {
+    attachTo = componentConfig.terminal.ATTACH_COMPONENT_TO;
+}catch(e){
+    console.error(e);
+    console.error('Warning: componentConfig.terminal.ATTACH_COMPONENT_TO not set. Please ensure PHP is serving the config correctly.');
+}
 
 initActions(store.dispatch, TERMINAL_URL);
 getTerminalData();
@@ -22,9 +38,9 @@ ReactDOM.render(
 	<Provider store={store} >
 		<Router history={browserHistory}>
 			<Route path="/" component={App}>
-				<Route path="*" component={Index} />
+				<Route path="terminal" component={Index} />
 			</Route>
 		</Router>
 	</Provider>
-  , document.querySelector(ATTACH_COMPONENT_TO)
+  , document.querySelector(attachTo)
 );
